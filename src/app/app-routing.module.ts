a1c722e5b0ed54fedb4f10961d5011746c778891
qq-zhong <qq-zhong@cs.sfu.ca>
2024-09-24 15:32:27 -0700

    trying to publish page with sub pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Game1Component } from './game1/game1.component'; //added
+import { Game2Component } from './game2/game2.component'; //added
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+
+// const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'game1', component: Game1Component },
+  { path: 'game2', component: Game2Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Game1Component } from './game1/game1.component';
+import { Game2Component } from './game2/game2.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Game1Component,
+    Game2Component,
+    MainComponent
   ],
   imports: [
     BrowserModule,
